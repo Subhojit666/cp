5805de7c1901e4b9991abfeb3955e7e477410c3b
Void23 <96782328+Subhojit666@users.noreply.github.com>
2024-02-26 20:34:06 +0530

    Create RemoveDuplicates.cpp

---
+++ RemoveDuplicates.cpp
@@ -0,0 +1,25 @@
+Node* removeDuplicates(Node* head) {
+    if (head == NULL) {
+        return NULL;
+    }
+
+    unordered_set<int> seen;
+    Node* prev = NULL;
+    Node* curr = head;
+
+    while (curr != NULL) {
+        if (seen.find(curr->data) != seen.end()) {
+            // If current node's data is already seen, it's a duplicate
+            prev->next = curr->next;
+            delete curr;
+            curr = prev->next;
+        } else {
+            // If not seen, add it to set and move forward
+            seen.insert(curr->data);
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+
+    return head;
+}
