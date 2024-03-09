ce4e54d215a3372b4992688da38d33641a2d200f
Void23 <96782328+Subhojit666@users.noreply.github.com>
2024-03-09 11:11:51 +0530

    Create Queue Reversal.cpp

---
+++ Queue Reversal.cpp
@@ -0,0 +1,21 @@
+Link:https://www.geeksforgeeks.org/problems/queue-reversal/1
+Code:
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+        stack<int> s;
+        while(!q.empty()){
+            int element = q.front();
+            q.pop();
+            s.push(element);
+        }
+        while(!s.empty()){
+            int element = s.top();
+            s.pop();
+            q.push(element);
+        }
+        return q;
+    }
+};
