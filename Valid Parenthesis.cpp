6a5393d4ab9ecad6f56fc4e433fb7605332cf373
Void23 <96782328+Subhojit666@users.noreply.github.com>
2024-03-11 20:16:21 +0530

    Create Valid Parenthesis.cpp

---
+++ Valid Parenthesis.cpp
@@ -0,0 +1,36 @@
+Link:https://www.codingninjas.com/studio/guided-paths/data-structures-algorithms/content/651074/offering/10442135?leftPanelTabValue=SUBMISSION&customSource=studio_nav
+Code:
+bool isValidParenthesis(string expression)
+{
+  	 stack<char> s;
+     for(int i=0; i<expression.length(); i++) {
+         char ch = expression[i];
+         if(ch == '(' || ch == '{' || ch == '['){
+             s.push(ch);
+         }
+         else
+         {
+             if(!s.empty()) {
+                  char top = s.top();
+                  if( (ch == ')' && top == '(') || 
+                     ( ch == '}' && top == '{') || 
+                     (ch == ']' && top == '[') ) {
+                      s.pop();
+                  }
+                 else
+                 {
+                     return false;
+                 }
+             }
+             else
+             {
+                 return false;
+             } 
+         }  
+     }
+    
+    if(s.empty())
+        return true;
+    else
+        return false;
+}
